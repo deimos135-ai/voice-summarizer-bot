d82225b1c13804ee4c7b29875dab00af2f26b240
deimos135-ai <ubnt5000@gmail.com>
2025-09-15 15:50:52 +0300

    Update util.py

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 import os
 import pytz
-from datetime import datetime
+from datetime import datetime, time, timedelta
 
 TZ = pytz.timezone(os.getenv("TZ", "Europe/Kyiv"))
 
@@ -12,3 +12,10 @@ def today_bounds():
     start = TZ.localize(datetime(n.year, n.month, n.day, 0, 0, 0))
     end = TZ.localize(datetime(n.year, n.month, n.day, 23, 59, 59))
     return start, end
+
+def next_run_at(hour=20, minute=0, second=0):
+    now = now_tz()
+    target = now.replace(hour=hour, minute=minute, second=second, microsecond=0)
+    if now >= target:
+        target = target + timedelta(days=1)
+    return target
